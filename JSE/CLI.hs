1bd3829e6b29ffa7c925c527a3fd953c71c2372e
Michael Xavier <michael@michaelxavier.net>
2011-09-06 19:31:34 -0700

    add filterspecs to cli help

--- JSE/CLI.hs
+++ JSE/CLI.hs
@@ -6,6 +6,7 @@ import JSE (Config(..), readFilterSpec)
 
 import Data.Data (Data)
 import Data.Enumerator.Binary (enumFile, enumHandle)
+import Data.List (intersperse)
 import Data.Text (pack)
 import Data.Typeable (Typeable)
 import System.Console.CmdArgs.Implicit ((&=),
@@ -34,9 +35,18 @@ annotations = record CLIConfig {rFields = def, ignoreC = False, inFile = Nothing
                      [ rFields    := def     += help "Fields to return. Returns all if not specified." += explicit += typ "FIELD" += name "f",
                       ignoreC     := False   += help "Case sensitive (defaults insensitive)" += explicit += name "c",
                       inFile      := Nothing += typFile += explicit += name "i" += help "Filename (stdin otherwise)",
-                      filterSpecs := def     += args]
+                      filterSpecs := def     += typ "FILTERSPEC" += args]
                       += program "jse"
-                      += summary "JSON Stream Editor"
+                      += summary summ
+  where summ = concat $ intersperse "\n" [
+                "JSON Stream Editor",
+                "Filter specs are a pair of object key and value, separated by a colon.",
+                "Filter specs may be specified as full strings, regular expressions or JSON values:",
+                "",
+                "Full string: somefield:\"full string to look for\"",
+                "JSON value: somefield:null",
+                "Regex value: somefield:/some substring/"
+               ]
 
 convertConfig :: CLIConfig -> Config
 convertConfig clc = Config { returnFields = map pack $ rFields clc,
